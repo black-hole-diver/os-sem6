b02e41ba50d73651088306b7fdf5c94bab53b73d
Wittawin Panta <wittawinpanta@Wittawins-MacBook-Air.local>
2023-03-04 22:30:03 +0700

    feat: added task to 1_3.c

---
+++ 1_3.c
@@ -0,0 +1,6 @@
+// Write a C program which declares a char array variable and allocate 80 bytes for it.
+// (char word[80] or char* word=(char*)malloc(80);)
+// Read a word from the keyboard with scanf/fgets functions into the variable.
+// Decide the length of it with strlen built in function! (Do not forget about including string.h!
+
+// Is there any difference between the usage of scanf and fgets? How can you read in more then one word?)
