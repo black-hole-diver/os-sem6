d11a33e7869669bfa1c151aba0b4dfff40527ba8
Wittawin Panta <wittawinpanta@Wittawins-MacBook-Air.local>
2023-03-04 22:36:02 +0700

    fix: error on malloc

--- 1_3.c
+++ 1_3.c
@@ -3,4 +3,17 @@
 // Read a word from the keyboard with scanf/fgets functions into the variable.
 // Decide the length of it with strlen built in function! (Do not forget about including string.h!
 
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char word[80];
+    printf("Enter a word");
+    fgets(word, 80, stdin);
+
+    int len = strlen(word);
+    printf("The word is %d char long.\n", len);
+    return 0;
+}
+
 // Is there any difference between the usage of scanf and fgets? How can you read in more then one word?)
